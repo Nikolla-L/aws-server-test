7e8bc40d7f6763f2e575de2d34a86e7f862e0603
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-03-03 11:17:49 +0400

    some change

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
+
 const app = express()
 
 const port = 3001
@@ -7,6 +10,9 @@ app.get('/', (req, res) => {
     res.send('That is it, got alll out')
 })
 
-app.listen(port, () => {
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app).listen(port, () => {
     console.log('port somethinbg something')
 })
